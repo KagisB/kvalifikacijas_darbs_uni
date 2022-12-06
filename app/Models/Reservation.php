2a90835e67bfb6fcde6cc85236cad4a333e05c22
Kārlis Bošs <kb16151@edu.lu.lv>
2022-12-06 20:56:23 +0200

    Reservation model methods

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,6 +2,7 @@
 
 namespace app\Models;
 
+use App\Models\DBConnection;
 use Datetime;
 
 class Reservation{
@@ -16,6 +17,15 @@ class Reservation{
         /*
          * Izveidot rezervāciju no padotajiem datiem
          * */
+        $connection = (new DBConnection())->createConnection();
+        $query = $connection->prepare('INSERT INTO Reservations VALUES (?,?,?,?,?)');
+        $code = $this->generateRandomString();
+        $timeFrom = date('Y-m-d H:i:s',$from->getTimestamp());
+        $timeTill = date('Y-m-d H:i:s',$till->getTimestamp());
+        $query->bind_param('iisss', $user_id, $space_id, $timeFrom, $timeTill, $code);
+        $query->execute();
+        $connection->close();
+        return $query;
     }
 
     public function getReservationsByUserId(int $user_id) : array
@@ -23,6 +33,19 @@ class Reservation{
         /*
          * atgriezt visas rezervācijas noteiktā periodā? kuras rezervējis konkrēts lietotājs
          * */
+        $connection = (new DBConnection())->createConnection();
+        $query = $connection->prepare('SELECT id FROM Reservations WHERE user_id = ?');
+        $query->bind_param('i', $user_id);
+        $query->execute();
+        $connection->close();
+        $result = $query->get_result();
+        $reservationIds= null;
+        while($row = $result->fetch_assoc())
+        {
+            $reservationIds[] = $row['id'];
+        }
+        $connection->close();
+        return $reservationIds;
     }
 
     public function getReservationsInTimePeriod(int $space_id,Datetime $from, Datetime $till) : array
@@ -30,6 +53,23 @@ class Reservation{
         /*
          * Iegūst rezervācijas noteiktajā periodā noteiktai stāvvietai
          * */
+        $connection = (new DBConnection())->createConnection();
+        $query = $connection->prepare(
+            'SELECT id FROM Reservations WHERE space_id = ?
+                    AND from > ? AND till < ?');
+        $timeFrom = date('Y-m-d H:i:s',$from->getTimestamp());
+        $timeTill = date('Y-m-d H:i:s',$till->getTimestamp());
+        $query->bind_param('iss', $space_id, $timeFrom, $timeTill);
+        $query->execute();
+        $connection->close();
+        $result = $query->get_result();
+        $reservationIds= null;
+        while($row = $result->fetch_assoc())
+        {
+            $reservationIds[] = $row['id'];
+        }
+        $connection->close();
+        return $reservationIds;
     }
 
     public function checkUserReservation(int $user_id,Datetime $from, Datetime $till) : bool
@@ -37,10 +77,46 @@ class Reservation{
         /*
          * Pārbaudīt, vai lietotājam ir rezervācija noteiktajā laika periodā
          * */
+        $connection = (new DBConnection())->createConnection();
+        $query = $connection->prepare(
+            'SELECT id FROM Reservations WHERE user_id = ?
+                    AND from > ? AND till < ?');
+        $timeFrom = date('Y-m-d H:i:s',$from->getTimestamp());
+        $timeTill = date('Y-m-d H:i:s',$till->getTimestamp());
+        $query->bind_param('iss', $space_id, $timeFrom, $timeTill);
+        $query->execute();
+        $connection->close();
     }
 
-    public function checkSpaceReservation(Datetime $timeNow) : bool
+    public function checkSpaceReservation(int $space_id, Datetime $timeNow) : bool
     {
+        $connection = (new DBConnection())->createConnection();
+        $query = $connection->prepare(
+            'SELECT id FROM Reservations WHERE space_id = ?
+                    AND from > ? AND till < ?');
+        $timeNow = date('Y-m-d H:i:s',$timeNow->getTimestamp());
+        $query->bind_param('is', $space_id, $timeNow);
+        $query->execute();
+        $connection->close();
+        if($query->result_metadata())
+        {
+            return true;
+        }
+        return false;
+    }
 
+    public function generateRandomString($length = 7) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            if($i===3)
+            {
+                $randomString .= '-';
+                continue;
+            }
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
