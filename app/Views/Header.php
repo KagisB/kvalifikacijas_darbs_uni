b36f9efc2cb0d101a1c7a170510f17c502a5aba7
Kārlis Bošs <kb16151@edu.lu.lv>
2022-12-22 23:03:08 +0200

    added a header file

---
+++ app/Views/Header.php
@@ -0,0 +1,21 @@
+<html lang="lv">
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.2.js"
+            integrity="sha256-pkn2CUZmheSeyssYw3vMp1+xyub4m+e+QK4sQskvuo4="
+            crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
+<section class="header">
+    <div class="container">
+        <nav id="linkMenu" class="nav justify-content-end">
+            <a class="nav-link col-sm" href="Index.php">Atpakaļ uz galveno lapu</a>
+            <a class="nav-link col-sm" href="ParkingLotList.php">Autostāvlaukumi</a>
+            <a id="logIn" class="nav-link visible col-sm" href="Login.php">Ieiet sistēmā</a>
+            <a id="logOut" class="nav-link col-sm invisible">Iziet no sistēmas</a>
+            <a id="userProfile" class="nav-link col-sm invisible" href="UserProfileView.php">Lietotāja profils</a>
+        </nav>
+    </div>
+</section>
+
